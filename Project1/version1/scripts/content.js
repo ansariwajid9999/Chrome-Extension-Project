d74e08ccebf0089dd321f6b5584375aa7e272267
Wajid <ansariwajid9999@gmail.com>
2023-11-12 01:27:57 +0530

    second commit

--- Project1/version1/scripts/content.js
+++ Project1/version1/scripts/content.js
@@ -1,3 +1,8 @@
+// In Dom tree sabse bahar hota h document fir body baad me sab kuchh
+
+// we are adding the button in the body, so we remove body, create button
+// , abhi page ka hissa nahi h , to id set karde , body me add kar lete h 
+// page me button dikhega nahi directly , inspect -> element -> ekdum nechhe
 let body = document.querySelector("body");
 
 let btnBQ = document.createElement("button");
@@ -5,12 +10,50 @@ btnBQ.setAttribute("id", "btnBQ");
 btnBQ.addEventListener("click", doSomething);
 body.appendChild(btnBQ);
 
+let speechRecognition = new webkitSpeechRecognition();
+speechRecognition.continous = true;
+speechRecognition.interimResults = true;
+speechRecognition.lang = "en-us";
+
+// The movement you take pause after speaking onresult event is fired and whatever you said is collected in transcript.
+
+let transcript = "";
+speechRecognition.onresult = function(event){
+  transcript = "";
+  for( let i = 0; i< event.results.length; ++i) {
+    transcript += event.results[i][0].transcript;
+  }
+};
+
+// document.addEventListener('keypress', handleKbd);
+
+// function handleKbd(event) {
+//   if (event.shiftKey && event.altKey && event.code === 'KeyQ') {
+//     btnBQ.click();
+//   }
+// }
+
+// Add event listener to the document to listen for keydown events
+document.addEventListener('keydown', function(event) {
+  // Check if the key combination is Ctrl + Alt + K
+  if (event.ctrlKey && event.altKey && event.key === 'K') {
+    // Call the function when the shortcut is pressed
+    btnBQ.click();
+  }
+});
+
 function doSomething(){
   if(btnBQ.hasAttribute("listening") === false){
     btnBQ.setAttribute("listening", true);
-    console.log("I am all ears");
+    speechRecognition.start();
   } else {
     btnBQ.removeAttribute("listening");
-    console.log("I am not listening anymore");
+    speechRecognition.stop();
+    const myPopup = new Popup({
+      id: "my-popup",
+      title: "Here is what you said:",
+      content: transcript,
+    });
+    myPopup.show();
   }
 }
